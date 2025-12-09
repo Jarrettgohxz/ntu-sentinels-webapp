b51196edc33cfc919f3ae6f852debafe197d6dff
jarrettgohxz <jarrettgoh.xz@gmail.com>
2025-12-09 23:40:23 +0800

    # Updated navbar to have underline of current active page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 import { RiMenu2Fill, RiCloseFill } from "react-icons/ri";
 
 function Header() {
+  const [page, setPage] = useState();
+
   const handleOpenMenu = () => {
     const menuList = document.querySelector(".main-nav");
     menuList.classList.add("is-open");
@@ -25,31 +28,50 @@ function Header() {
 
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link
+                className={`nav-link ${page == "home" ? "underline" : ""}`}
+                to="/"
+                onClick={() => setPage("home")}
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about-us">
+              <Link
+                className={`nav-link ${page == "about-us" ? "underline" : ""}`}
+                to="/about-us"
+                onClick={() => setPage("about-us")}
+              >
                 About Us
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/events">
+              <Link
+                className={`nav-link ${page == "events" ? "underline" : ""}`}
+                to="/events"
+                onClick={() => setPage("events")}
+              >
                 Events
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact-us">
+              <Link
+                className={`nav-link ${
+                  page == "contact-us" ? "underline" : ""
+                }`}
+                to="/contact-us"
+                onClick={() => setPage("contact-us")}
+              >
                 Contact Us
               </Link>
             </li>
           </ul>
         </div>
+
+        <div className="divider" />
       </nav>
 
       <RiMenu2Fill className="icon-menu" onClick={handleOpenMenu} />
-      <div className="divider" />
     </>
   );
 }
